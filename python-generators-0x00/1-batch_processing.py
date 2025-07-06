805fb7b549e668a260da7c87298c9c6815f08126
Mohameddek1 <mohadek2004@gmail.com>
2025-07-06 20:18:24 +0300

    commit

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,23 +1,30 @@
 import mysql.connector
 
-def stream_users():
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="moha123",  # Replace with your MySQL root password
-            database="ALX_prodev"
-        )
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
+def stream_users_in_batches(batch_size):
+    offset = 0
+    while True:
+        users = fetch_batch(batch_size, offset)
+        if not users:
+            break
+        yield users
+        offset += batch_size
 
-        for row in cursor:
-            yield row
+def fetch_batch(batch_size, offset):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="moha123",  # Replace with your actual MySQL password
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {batch_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
 
-    except mysql.connector.Error as err:
-        print(f"Database error: {err}")
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
