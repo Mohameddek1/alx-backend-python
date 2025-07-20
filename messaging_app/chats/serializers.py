15f7a3ef0f6fe2cae08f61f6effd9b1f2b398578
Mohameddek1 <mohadek2004@gmail.com>
2025-07-20 18:17:32 +0300

    django

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'role', 'phone_number']
@@ -10,16 +14,26 @@ class UserSerializer(serializers.ModelSerializer):
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
 
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
 
+    def validate(self, data):
+        if not data.get('message_body'):
+            raise ValidationError("Message body cannot be empty.")
+        return data
+
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_messages(self, obj):
+        messages = obj.messages.all().order_by('sent_at')
+        return MessageSerializer(messages, many=True).data
