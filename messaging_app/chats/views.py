09a2610934cdd20f0ff3e2f4cc6adc7e2b8abdc8
Mohameddek1 <mohadek2004@gmail.com>
2025-07-20 18:31:01 +0300

    django

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
@@ -9,6 +9,9 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all().prefetch_related('participants', 'messages')
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__username']
+    ordering_fields = ['created_at']
 
     def create(self, request, *args, **kwargs):
         participants = request.data.get('participants')
@@ -19,7 +22,6 @@ class ConversationViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        # Ensure the requesting user is included as a participant
         if request.user.id not in participants:
             participants.append(request.user.id)
 
@@ -35,11 +37,13 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all().select_related('conversation', 'sender')
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['content']
+    ordering_fields = ['timestamp']
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        # Automatically attach the sender as the logged-in user
         serializer.save(sender=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
