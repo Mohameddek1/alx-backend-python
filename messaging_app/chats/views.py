535743c7414a0075e797bb231d18a20c954819d7
Mohameddek1 <mohadek2004@gmail.com>
2025-07-20 18:22:52 +0300

    django

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all().prefetch_related('participants', 'messages')
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        participants = request.data.get('participants')
+        if not participants or len(participants) < 2:
+            return Response({'error': 'A conversation must have at least two participants.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participants)
+        conversation.save()
+
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all().select_related('conversation', 'sender')
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
