2fbe8c21144c1e1d0799b5cb366a57f651e4e542
Mohameddek1 <mohadek2004@gmail.com>
2025-07-27 13:44:50 +0300

    middle

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -12,3 +12,19 @@ class RequestLoggingMiddleware:
         # Debug print to verify middleware is running
         print(f"Logging request: {request.path}")
         return self.get_response(request)
+
+
+from datetime import time
+from django.http import HttpResponseForbidden
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_time = datetime.now().time()
+        start_time = time(18, 0, 0)
+        end_time = time(21, 0, 0)
+        if not (start_time <= current_time <= end_time):
+            return HttpResponseForbidden("Access to chats is restricted between 6PM and 9PM only.")
+        return self.get_response(request)
