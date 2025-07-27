039fd200dc4aca381684202ec3db6f46bf4c200c
Mohameddek1 <mohadek2004@gmail.com>
2025-07-27 14:10:21 +0300

    middle

--- Django-Middleware-0x03/Django_Middleware_0x03/settings.py
+++ Django-Middleware-0x03/Django_Middleware_0x03/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.OffensiveLanguageMiddleware',
+    'chats.middleware.RolepermissionMiddleware',
 ]
 
 ROOT_URLCONF = 'Django_Middleware_0x03.urls'
