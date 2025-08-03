8d23e9bad91247bc25f24f95c9426cb000803089
Mohameddek1 <mohadek2004@gmail.com>
2025-08-03 19:31:28 +0300

    latest

---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Message, Notification
+
+User = get_user_model()
+
+class SignalTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='alice', password='pass123')
+        self.user2 = User.objects.create_user(username='bob', password='pass123')
+
+    def test_notification_created_on_message(self):
+        message = Message.objects.create(sender=self.user1, receiver=self.user2, content='Hello!')
+        self.assertEqual(Notification.objects.count(), 1)
