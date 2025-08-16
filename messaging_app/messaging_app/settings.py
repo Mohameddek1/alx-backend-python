f192603e9bfb35b31ffd7476700a405670e37490
Mohameddek1 <mohadek2004@gmail.com>
2025-08-16 14:58:45 +0300

    new

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Caching
 CACHES = {
     'default': {
@@ -84,10 +86,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.environ.get('MYSQL_DATABASE', 'messagingdb'),
+    'USER': os.environ.get('MYSQL_USER', 'messaginguser'),
+    'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'messagingpass'),
+    'HOST': os.environ.get('MYSQL_HOST', 'db'),
+    'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
 }
+
 
 
 # Django REST Framework configuration
